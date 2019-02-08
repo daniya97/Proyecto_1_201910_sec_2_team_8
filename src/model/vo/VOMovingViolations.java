60f1c3430a409d046378098333cc64c71b561e16
Luis Eduardo Yamin Lacouture <lyamin@uniandes.edu.co>
2019-02-08 13:19:47 -0500

    VO MOving Clase actualizada

--- src/model/vo/VOMovingViolations.java
+++ src/model/vo/VOMovingViolations.java
@@ -5,22 +5,53 @@ package model.vo;
  */
 public class VOMovingViolations {
 
+
+	/**
+	 * Atributos de la infracción
+	 */
+	private String iD;
+	private String location;
+	private int totalPaid;
+	private String accidentIndicator;
+	private String ticketIssueDate;
+	private String violationCode;
+	private String violationDesc;
+	
+	
+	/**
+	 * Constructor. Recibe los argumentos de la infracción a través del archivo CSV
+	 */
+	public VOMovingViolations(String[] linea){
+	
+	
+		iD = linea[0];
+		location = linea[2];
+		totalPaid = Integer.parseInt(linea[9]);
+		accidentIndicator = linea[12];
+		ticketIssueDate = linea[13];
+		violationCode = linea[14];
+		violationDesc = linea[15];
+		
+	}
+	
+	
+	
 	
 	/**
 	 * @return id - Identificador Ãºnico de la infracciÃ³n
 	 */
-	public int objectId() {
+	public String objectId() {
 		// TODO Auto-generated method stub
-		return 0;
+		return iD;
 	}	
 	
 	
 	/**
-	 * @return location - DirecciÃ³n en formato de texto.
+	 * @return location - Dirección en formato de texto.
 	 */
 	public String getLocation() {
 		// TODO Auto-generated method stub
-		return "";
+		return location;
 	}
 
 	/**
@@ -28,15 +59,15 @@ public class VOMovingViolations {
 	 */
 	public String getTicketIssueDate() {
 		// TODO Auto-generated method stub
-		return "";
+		return ticketIssueDate;
 	}
 	
 	/**
-	 * @return totalPaid - Cuanto dinero efectivamente pagÃ³ el que recibiÃ³ la infracciÃ³n en USD.
+	 * @return totalPaid - Cuanto dinero efectivamente pagó el que recibió la infracción en USD.
 	 */
 	public int getTotalPaid() {
 		// TODO Auto-generated method stub
-		return 0;
+		return totalPaid;
 	}
 	
 	/**
@@ -44,14 +75,19 @@ public class VOMovingViolations {
 	 */
 	public String  getAccidentIndicator() {
 		// TODO Auto-generated method stub
-		return "";
+		return accidentIndicator;
 	}
 		
 	/**
-	 * @return description - DescripciÃ³n textual de la infracciÃ³n.
+	 * @return description - Descripción textual de la infracciÃ³n.
 	 */
 	public String  getViolationDescription() {
 		// TODO Auto-generated method stub
-		return "";
+		return violationDesc;
+	}
+	
+	public String  getViolationCode() {
+		// TODO Auto-generated method stub
+		return violationCode;
 	}
 }
